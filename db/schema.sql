49497bb788f3dbf3e0558728b910cf8f08bfb83d
RyanOfEastview <ryanmccarthy613@gmail.com>
2022-09-16 17:14:13 -0400

    db, schema and seeds completed and operational

--- db/schema.sql
+++ db/schema.sql
@@ -1,14 +1,14 @@
 CREATE TABLE department (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     dept VARCHAR(30)
-    );
+);
 
 CREATE TABLE role (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30),
     salary DECIMAL(30),
     department_id INTEGER
-    );
+);
 
 CREATE TABLE employee (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
