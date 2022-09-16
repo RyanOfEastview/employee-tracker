aacdff19cceadf3d5e9655d29815374fd8c1c8ea
RyanOfEastview <ryanmccarthy613@gmail.com>
2022-09-16 16:59:34 -0400

    added CRUD routes

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const mysql = require("mysql2");
 const express = require("express");
+const inputCheck = require("./utils/inputCheck");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -21,8 +22,83 @@ const db = mysql.createConnection(
   console.log("Conneted to the biz database")
 );
 
-db.query(`SELECT * FROM employee`, (err, rows) => {
-  console.log(rows);
+// GET list of all employees
+app.get("/api/employee", (req, res) => {
+  const sql = `SELECT * FROM employee`;
+
+  db.query(sql, (err, rows) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: "success",
+      data: rows,
+    });
+  });
+});
+
+// Get a single employee
+app.get("/api/employee/:id", (req, res) => {
+  const sql = `SELECT * FROM employee WHERE id = ?`;
+  const params = [req.params.id];
+
+  db.query(sql, params, (err, row) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: "success",
+      data: row,
+    });
+  });
+});
+
+// Delete a employee
+app.delete("/api/employee/:id", (req, res) => {
+  const sql = `DELETE FROM employee WHERE id = ?`;
+  const params = [req.params.id];
+
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.statusMessage(400).json({ error: res.message });
+    } else if (!result.affectedRows) {
+      res.json({
+        message: "employee not found",
+      });
+    } else {
+      res.json({
+        message: "deleted",
+        changes: result.affectedRows,
+        id: req.params.id,
+      });
+    }
+  });
+});
+
+// Create an employee
+app.post("/api/employee", ({ body }, res) => {
+  const errors = inputCheck(body, "first_name", "last_name");
+  if (errors) {
+    res.status(400).json({ error: errors });
+    return;
+  }
+
+  const sql = `INSERT INTO employee (first_name, last_name)
+  VALUES (?,?)`;
+  const params = [body.first_name, body.last_name];
+
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: "success",
+      data: body,
+    });
+  });
 });
 
 // Default response for any other request (Not Found)
